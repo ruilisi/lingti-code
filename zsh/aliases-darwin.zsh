cbcb693a077d41f7b496f6de1d8d4787281067fb
hophacker <jiefeng.hopkins@gmail.com>
2026-04-23 17:22:01 +0800

    feat: add chrome-cdp alias to launch Chrome with remote debugging port

--- zsh/aliases-darwin.zsh
+++ zsh/aliases-darwin.zsh
@@ -3,3 +3,6 @@ if [[ "$(uname)" != "Darwin" ]]; then
 fi
 
 alias list_identities='security find-identity -v -p codesigning'
+
+# Chrome with CDP (remote debugging for automation/MCP)
+alias chrome-cdp='open -a "Google Chrome" --args --remote-debugging-port=9222 --restore-last-session'
